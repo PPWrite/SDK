9bfe6b71d3b36771f7d8ba23f4d117afeca32cc1
dadou <linhw@ppwrite.com>
2016-08-02 14:46:54 +0800

    8-2 update windows_SDK C++

--- Windows_SDK/C++/inc/UsbDevInterface.h
+++ Windows_SDK/C++/inc/UsbDevInterface.h
@@ -31,7 +31,7 @@ typedef struct sPenInfo
 }PEN_INFO;
 
 // 返回usb数据回调类型
-typedef void (*usbDataCallBack)(const char* pUsbData, const sPenInfo& penInfo);
+typedef void ( CALLBACK *usbDataCallBack)(const char* pUsbData, const sPenInfo& penInfo, void* context);
 // 返回usb设备的插拔状态
 typedef void (*usbStatusCallBack)(int nDevstatus, void* context);
 
@@ -57,7 +57,7 @@ public:
     // parameter: [IN] pFunc 指定返回数据的回调 默认为null
     // return: bool 设备是否打开成功
     // ********************************************************
-    virtual bool _openSpecUsbDevByPid(IN const std::vector<std::string>& vecPid, IN usbDataCallBack pFunc = NULL) = 0;
+    virtual bool _openSpecUsbDevByPid(IN const std::vector<std::string>& vecPid, IN usbDataCallBack pFunc = NULL, IN void* context = NULL) = 0;
 
     // *******************************************
     // summary: 关闭设备资源
@@ -78,16 +78,15 @@ public:
     virtual usbStatusCallBack setDevStatusHandler(usbStatusCallBack newHandler, void* pUsbDevStatusContext = NULL) = 0;
 };
 
-extern "C" DLL_PORT_TYPE CUsbDevInterface* WINAPI createObj();
-extern "C" DLL_PORT_TYPE void WINAPI deleteObj(void* p);
+// createObj deleteObj说明
+// createObj函数提供获取设备接口类的功能, 获取到设备对象后可调用设备相关接口
+// deleteObj函数提供程序退出时释放相关资源
+extern "C" DLL_PORT_TYPE CUsbDevInterface*  createObj();
+extern "C" DLL_PORT_TYPE void  deleteObj(void* p);
 
-//////////////////////////////////////////////////////////////////////////
-/*打开设备接口*/
-//////////////////////////////////////////////////////////////////////////
-extern "C" DLL_PORT_TYPE bool WINAPI _extern_openSpecUsbDevByPid(char* pAarryPid[], unsigned int nAarrySize, usbDataCallBack pFunc);
-
-//////////////////////////////////////////////////////////////////////////
-/*关闭设备接口*/
-//////////////////////////////////////////////////////////////////////////
-extern "C" DLL_PORT_TYPE bool WINAPI _extern_CloseUsbDev();
+// _extern_openSpecUsbDevByPid _extern_CloseUsbDev说明
+// _extern_openSpecUsbDevByPid函数提供根据设备pid来打开设备, 无需使用上面的对象来调用相关接口
+// _extern_CloseUsbDev函数提供关闭设备的功能
+extern "C" DLL_PORT_TYPE bool  _extern_openSpecUsbDevByPid(char* pAarryPid[], unsigned int nAarrySize, usbDataCallBack pFunc);
+extern "C" DLL_PORT_TYPE bool  _extern_CloseUsbDev();
  
